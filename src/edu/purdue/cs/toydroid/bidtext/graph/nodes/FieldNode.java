012db749e099ea5b01d066688c4e02cfc3dd397c
Jianjun Huang <hjjandy@gmail.com>
2015-11-23 16:21:45 -0500

    FieldNode with path

---
+++ src/edu/purdue/cs/toydroid/bidtext/graph/nodes/FieldNode.java
@@ -0,0 +1,54 @@
+package edu.purdue.cs.toydroid.bidtext.graph.nodes;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.ibm.wala.ipa.slicer.Statement;
+
+public class FieldNode {
+
+	private int field;
+	private List<Statement> fPath, bPath;
+
+	public FieldNode(int s) {
+		field = s;
+		fPath = new LinkedList<Statement>();
+		bPath = new LinkedList<Statement>();
+	}
+
+	public int value() {
+		return field;
+	}
+
+	public boolean addForwardPath(Statement p) {
+		return fPath.add(p);
+	}
+
+	public boolean addBackwardPath(Statement p) {
+		return bPath.add(p);
+	}
+
+	public Iterator<Statement> iterateForwardPath() {
+		return fPath.iterator();
+	}
+
+	public Iterator<Statement> iterateBackwardPath() {
+		return bPath.iterator();
+	}
+
+	public boolean equals(Object obj) {
+		if (obj instanceof FieldNode) {
+			FieldNode that = (FieldNode) obj;
+			if (field == that.field && fPath.equals(that.fPath)
+					&& bPath.equals(that.bPath)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		return (field + fPath.hashCode() * 7 + bPath.hashCode() * 11);
+	}
+}
