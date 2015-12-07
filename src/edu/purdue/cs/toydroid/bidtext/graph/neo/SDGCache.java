1e6c6ae8b6f47d3de3af569cf50ed3b3d5f0920f
Jianjun Huang <hjjandy@gmail.com>
2015-12-07 13:34:17 -0500

    cache stmt with const or field

--- src/edu/purdue/cs/toydroid/bidtext/graph/neo/SDGCache.java
+++ src/edu/purdue/cs/toydroid/bidtext/graph/neo/SDGCache.java
@@ -1,8 +1,10 @@
 package edu.purdue.cs.toydroid.bidtext.graph.neo;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +13,16 @@ import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.ipa.slicer.NormalReturnCaller;
+import com.ibm.wala.ipa.slicer.NormalStatement;
 import com.ibm.wala.ipa.slicer.ParamCaller;
+import com.ibm.wala.ipa.slicer.PhiStatement;
 import com.ibm.wala.ipa.slicer.Statement;
 import com.ibm.wala.ssa.SSAAbstractInvokeInstruction;
+import com.ibm.wala.ssa.SSAArrayLoadInstruction;
+import com.ibm.wala.ssa.SSAArrayStoreInstruction;
+import com.ibm.wala.ssa.SSAFieldAccessInstruction;
+import com.ibm.wala.ssa.SSAInstruction;
+import com.ibm.wala.ssa.SymbolTable;
 import com.ibm.wala.util.graph.Graph;
 
 import edu.purdue.cs.toydroid.utils.WalaUtil;
@@ -22,10 +31,12 @@ public class SDGCache {
 	private static Logger logger = LogManager.getLogger(SDGCache.class);
 	private Entrypoint entry;
 	private Map<CGNode, SDGSubCache> cg2cache;
+	private Set<Statement> cachedStmt;
 
 	public SDGCache(Entrypoint e) {
 		entry = e;
 		cg2cache = new HashMap<CGNode, SDGSubCache>();
+		cachedStmt = new HashSet<Statement>();
 	}
 
 	public Entrypoint entrypoint() {
@@ -36,26 +47,81 @@ public class SDGCache {
 		logger.info("Build SDG Cache...");
 		for (Statement stmt : sdg) {
 			Statement.Kind k = stmt.getKind();
-			if (k == Statement.Kind.PARAM_CALLER
-					&& 0 == sdg.getSuccNodeCount(stmt)) {
-				ParamCaller pcaller = (ParamCaller) stmt;
-				if (WalaUtil.isAPI(pcaller)) {
-					CGNode cgn = pcaller.getNode();
-					int value = pcaller.getValueNumber();
-					addCache(cgn, value, stmt);
-				}
-			} else if (k == Statement.Kind.NORMAL_RET_CALLER
-					&& 0 == sdg.getPredNodeCount(stmt)) {
-				NormalReturnCaller nrc = (NormalReturnCaller) stmt;
-				if (WalaUtil.isAPI(nrc)) {
-					CGNode cgn = nrc.getNode();
-					SSAAbstractInvokeInstruction inst = nrc.getInstruction();
-					int nUses = inst.getNumberOfUses();
-					for (int i = 0; i < nUses; i++) {
-						int use = inst.getUse(i);
-						addCache(cgn, use, stmt);
+			CGNode cgn = stmt.getNode();
+			SSAInstruction inst;
+			int nUses, idx;
+			SymbolTable symTable;
+			switch (k) {
+				case PHI:
+					PhiStatement phi = (PhiStatement) stmt;
+					symTable = cgn.getIR().getSymbolTable();
+					inst = phi.getPhi();
+					nUses = inst.getNumberOfUses();
+					for (idx = 0; idx < nUses; idx++) {
+						int v = inst.getUse(idx);
+						if (symTable.isConstant(v)
+								&& !symTable.isNullConstant(v)) {
+							cachedStmt.add(stmt);
+							break;
+						}
 					}
-				}
+					break;
+				case NORMAL:
+					NormalStatement nstmt = (NormalStatement) stmt;
+					symTable = cgn.getIR().getSymbolTable();
+					inst = nstmt.getInstruction();
+					if (inst instanceof SSAFieldAccessInstruction) {
+						cachedStmt.add(stmt);
+					} else if (inst instanceof SSAArrayStoreInstruction) {
+						SSAArrayStoreInstruction ainst = (SSAArrayStoreInstruction) inst;
+						int v = ainst.getValue();
+						if (symTable.isConstant(v)
+								&& !symTable.isNullConstant(v)) {
+							cachedStmt.add(stmt);
+						}
+					} else if (!(inst instanceof SSAArrayLoadInstruction)) {
+						nUses = inst.getNumberOfUses();
+						for (idx = 0; idx < nUses; idx++) {
+							int v = inst.getUse(idx);
+							if (symTable.isConstant(v)
+									&& !symTable.isNullConstant(v)) {
+								cachedStmt.add(stmt);
+								break;
+							}
+						}
+					}
+					break;
+				case PARAM_CALLER:
+					ParamCaller pcaller = (ParamCaller) stmt;
+					int value = pcaller.getValueNumber();
+					if (0 == sdg.getSuccNodeCount(stmt)
+							&& WalaUtil.isAPI(pcaller)) {
+						addCache(cgn, value, stmt);
+					}
+					symTable = cgn.getIR().getSymbolTable();
+					if (symTable.isConstant(value)
+							&& !symTable.isNullConstant(value)) {
+						cachedStmt.add(stmt);
+					}
+					break;
+				case NORMAL_RET_CALLER:
+					NormalReturnCaller nrc = (NormalReturnCaller) stmt;
+					inst = nrc.getInstruction();
+					nUses = inst.getNumberOfUses();
+					if (0 == sdg.getPredNodeCount(stmt) && WalaUtil.isAPI(nrc)) {
+						symTable = cgn.getIR().getSymbolTable();
+						for (idx = 0; idx < nUses; idx++) {
+							int use = inst.getUse(idx);
+							addCache(cgn, use, stmt);
+							if (symTable.isConstant(use)
+									&& !symTable.isNullConstant(use)) {
+								cachedStmt.add(stmt);
+							}
+						}
+					}
+					break;
+				default:
+					break;
 			}
 		}
 	}
